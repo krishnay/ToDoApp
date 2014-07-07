b22bb9a2d8f42ea2064d71dcc8fa7139a9c3d58b
krishna <krish.yalla@gmail.com>
2014-07-06 23:42:48 -0400

    Changes made to the module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,15 +11,16 @@
 angular.module('ToDo', [])
         .controller('todoController',['$scope', function($scope){
          $scope.todos =[
-            {'title': 'to build a todo app', 'done' : false}
+            {'title': 'to build a todo app','assignee' : 'krishna', 'done' : false}
          ];
          $scope.addTodo = function(){
-          $scope.todos.push({'title' :$scope.newTodo, 'done': false})
+          $scope.todos.push({'title' :$scope.newTodo, 'assignee' : $scope.newassigneetodo, 'done': false});
           $scope.newTodo = ''
+          $scope.newassigneetodo = ''
          }
          $scope.clearCompleted = function(){
           $scope.todos = $scope.todos.filter(function(item){
             return !item.done
-          })
+          });
          }
         }]);
